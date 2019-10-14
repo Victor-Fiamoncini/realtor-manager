2d44e0dfca15ac3b87b585e082c47919abed95f4
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2019-10-13 22:19:42 -0300

    Admin customization

--- btre/urls.py
+++ btre/urls.py
@@ -1,10 +1,15 @@
 # Imports
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 # Urls
 urlpatterns = [
-  path('admin/', admin.site.urls),
+  # Admin
+	path('admin/', admin.site.urls),
+	# '/'
 	path('', include('pages.urls')),
+	# /listings/
 	path('listings/', include('listings.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- listings/admin.py
+++ listings/admin.py
@@ -2,5 +2,14 @@
 from django.contrib import admin
 from .models import Listing
 
-# Add listings to dashboard
-admin.site.register(Listing)
+# ListingAdmin
+class ListingAdmin(admin.ModelAdmin):
+	list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
+	list_display_links = ('id', 'title')
+	list_filter = ('realtor',)
+	list_editable = ('is_published',)
+	search_fields = ('title', 'description', 'address', 'city', 'state', 'zipcode', 'price')
+	list_per_page = 25
+
+# Add to dashboard
+admin.site.register(Listing, ListingAdmin)
